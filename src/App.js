080501f4ab5fe1c1ddcfdb7deeb4e049c629ed4e
jeromson30 <jerome.corso@gmail.com>
2024-08-25 14:24:43 +0200

    style: create CSS & logo components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import Logo from './components/logo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo />
+        <nav aria-label='Primary navigation'>
+          <ul>
+            <li></li>
+          </ul>
+        </nav>
       </header>
+
+      <footer>
+
+      </footer>
     </div>
   );
 }
